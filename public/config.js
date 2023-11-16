a4b1781a3575861fc2a0d49e2c4cb4776a90cfcd
kjddskmksn <149243786+kjddskmksn@users.noreply.github.com>
2023-11-15 21:53:43 -0600

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: https://worker-white-feather-c934.katav55440.workers.dev"",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODFmNzg3MWI2NTgyYWE5YmIyZDA4ZWUxMWQxMDJhZiIsInN1YiI6IjY1M2VmNzQxYzhhNWFjMDExZDZkYjRkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v1m-ORHhdfobaqUr9eZr3jni20JDCu1iGWVbQODmpKw"
 };
